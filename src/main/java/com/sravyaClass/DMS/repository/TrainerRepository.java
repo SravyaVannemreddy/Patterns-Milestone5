0d0308006469a73abecc1693fd0502a556c6d273
Sravya Vannemreddy <143159657+SravyaVannemreddy@users.noreply.github.com>
2024-04-19 14:46:13 -0700

    Add files via upload

---
+++ src/main/java/com/sravyaClass/DMS/repository/TrainerRepository.java
@@ -0,0 +1,13 @@
+package com.sravyaClass.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.sravyaClass.DMS.Models.Trainer;
+
+
+/**
+ * @author S564547
+ */
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
