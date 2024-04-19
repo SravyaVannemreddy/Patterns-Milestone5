67d5da3d9b5fee8ec32225ee14328925779fa9fe
Sravya Vannemreddy <143159657+SravyaVannemreddy@users.noreply.github.com>
2024-04-19 14:22:36 -0700

    Add files via upload

---
+++ DogManagementSystem/src/main/java/com/sravyaClass/DMS/DogManagementSystemApplication.java
@@ -0,0 +1,17 @@
+package com.sravyaClass.DMS;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author S564547
+ */
+@SpringBootApplication
+public class DogManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogManagementSystemApplication.class, args);
+	}
+
+}
